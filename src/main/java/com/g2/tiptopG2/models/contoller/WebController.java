ef5163c6cf2900af39aa2d08799a56dd7e228077
mokrim <mokrimmohamed2016@gmail.com>
2024-08-09 14:09:03 +0200

    init project

---
+++ src/main/java/com/g2/tiptopG2/models/contoller/WebController.java
@@ -0,0 +1,12 @@
+package com.g2.tiptopG2.models.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Correspond à src/main/resources/templates/index.html pour Thymeleaf
+    }
+}
---
+++ src/test/java/com/g2/tiptopG2/TiptopG2ApplicationTests.java
@@ -0,0 +1,6 @@
+package com.g2.tiptopG2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
