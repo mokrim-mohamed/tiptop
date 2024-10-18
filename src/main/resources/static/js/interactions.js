77938ef760924aa6a68ebf95a8a70677e1fa4d8a
Driss Chahm <41614250+Dgiss@users.noreply.github.com>
2024-10-18 10:42:16 +0200

    update index

--- src/main/resources/static/js/interactions.js
+++ src/main/resources/static/js/interactions.js
@@ -7,7 +7,14 @@ document.querySelector('.btn-dark').addEventListener('click', function() {
 const logo = document.querySelector('.navbar-brand img');
 logo.addEventListener('mouseover', () => {
     logo.style.transform = 'rotate(360deg)';
+    logo.style.transition = 'transform 0.5s';
 });
 logo.addEventListener('mouseout', () => {
     logo.style.transform = 'rotate(0deg)';
 });
+
+// Cookie Banner Function
+function acceptCookies() {
+    document.getElementById('cookie-banner').style.display = 'none';
+    // Ici, vous pouvez ajouter un cookie pour mémoriser le choix de l'utilisateur
+}
