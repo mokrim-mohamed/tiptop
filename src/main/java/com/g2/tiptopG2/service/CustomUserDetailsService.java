1920fd504857b79060056c9ae475ed2652e19daa
mokrim <mokrimmohamed2016@gmail.com>
2024-10-21 21:48:28 +0200

    add role to login

--- src/main/java/com/g2/tiptopG2/service/CustomUserDetailsService.java
+++ src/main/java/com/g2/tiptopG2/service/CustomUserDetailsService.java
@@ -5,8 +5,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import com.g2.tiptopG2.service.IRoleService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.g2.tiptopG2.models.UserEntity;
+import com.g2.tiptopG2.models.RoleEntity;
+import com.g2.tiptopG2.dto.RoleDto;
+
 import org.springframework.stereotype.Service;
 import com.g2.tiptopG2.dto.UserDto;
 import java.util.Collections;
@@ -17,6 +22,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserServiceImp userService;
 
+    @Autowired
+    private IRoleService roleService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserDto byLogin = userService.findByEmail(username);
@@ -26,8 +34,9 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (byLogin == null) {
             throw new UsernameNotFoundException("user mot found");
         }
-        
-        return new User(byLogin.getEmail(),byLogin.getMotDePasse(),Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+        RoleDto role = roleService.findById(byLogin.getRoleId());
+
+        return new User(byLogin.getEmail(),byLogin.getMotDePasse(),Collections.singletonList(new SimpleGrantedAuthority(role.getRole())));
         
     }
 }
