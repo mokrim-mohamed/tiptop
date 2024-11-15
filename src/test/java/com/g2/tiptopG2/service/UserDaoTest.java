9d7e7c8e592b1aa6d71d149ec6a2b654e2fb0618
mokrim <mokrimmohamed2016@gmail.com>
2024-11-15 09:49:55 +0100

    test sonarQube

--- src/test/java/com/g2/tiptopG2/service/UserDaoTest.java
+++ src/test/java/com/g2/tiptopG2/service/UserDaoTest.java
@@ -7,13 +7,11 @@ import com.g2.tiptopG2.models.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
