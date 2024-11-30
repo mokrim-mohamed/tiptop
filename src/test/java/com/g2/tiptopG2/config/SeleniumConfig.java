02e749fd45d9c50f72602143ade22c172dbdaed5
mokrim <mokrimmohamed2016@gmail.com>
2024-11-30 12:06:11 +0100

    test fonctionnels

--- src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
+++ src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 @Configuration
 public class SeleniumConfig {
@@ -14,17 +13,6 @@ public class SeleniumConfig {
     public WebDriver webDriver() {
         // Configuration automatique du WebDriver pour Chrome
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        
-        // Options pour exécuter Chrome en mode headless (sans interface graphique)
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");  // Nécessaire pour le mode headless sur certains systèmes
-        options.addArguments("--window-size=1920x1080");  // Définit la taille de la fenêtre
-        options.addArguments("--no-sandbox");  // Nécessaire dans certains environnements (par exemple, dans Docker)
-        options.addArguments("--disable-dev-shm-usage");  // Permet d'éviter des erreurs de mémoire partagée dans certains environnements
-        options.addArguments("--remote-debugging-port=9222");  // Débogage à distance, utile pour les logs et diagnostics
-        
-        // Retourne une nouvelle instance de ChromeDriver avec les options spécifiées
-        return new ChromeDriver(options);
+        return new ChromeDriver();
     }
 }
