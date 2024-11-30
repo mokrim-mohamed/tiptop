61bb454d997673af3e6b370cba3ebc6abf2b3e60
mokrim <mokrimmohamed2016@gmail.com>
2024-11-30 13:23:00 +0100

    test fonctionnels

---
+++ src/test/java/com/g2/tiptopG2/fonctionnel/faqPageTest.java
@@ -0,0 +1,53 @@
+package com.g2.tiptopG2.fonctionnel;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class faqPageTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.get("https://test.wk-archi-o23-15m-g2.fr/faq");
+
+        // Attente explicite
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.tagName("h1")));
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void testHomePageTitle() {
+        WebElement titleElement = driver.findElement(By.tagName("h1"));
+        assertEquals("Questions Fréquemment Posées (FAQ)", titleElement.getText());
+    }
+}
