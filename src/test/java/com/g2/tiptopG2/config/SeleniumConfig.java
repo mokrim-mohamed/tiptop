83561f660c3ac8265758c675ad77477fd6cb6d4c
mokrim <mokrimmohamed2016@gmail.com>
2024-11-30 11:50:46 +0100

    test fonctionnels

--- src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
+++ src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @Configuration
 public class SeleniumConfig {
