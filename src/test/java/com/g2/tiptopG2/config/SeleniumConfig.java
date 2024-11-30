3e97e963539def5d90a7697459e327ed49927af0
mokrim <mokrimmohamed2016@gmail.com>
2024-11-30 12:08:21 +0100

    test fonctionnels

--- src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
+++ src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @Configuration
 public class SeleniumConfig {
@@ -13,6 +14,11 @@ public class SeleniumConfig {
     public WebDriver webDriver() {
         // Configuration automatique du WebDriver pour Chrome
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");  // Nécessaire pour le mode headless sur certains systèmes
+        options.addArguments("--window-size=1920x1080");  // Définit la taille de la fenêtre
+        options.addArguments("--no-sandbox");  // Impor
+        return new ChromeDriver(options);
     }
 }
