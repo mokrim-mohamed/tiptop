d5dee6ca811c366f837cb5a152efa555ae31ef77
mokrim <mokrimmohamed2016@gmail.com>
2024-11-30 12:59:25 +0100

    test fonctionnels

--- src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
+++ src/test/java/com/g2/tiptopG2/config/SeleniumConfig.java
@@ -3,6 +3,7 @@ package com.g2.tiptopG2.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +12,17 @@ public class SeleniumConfig {
 
     @Bean
     public WebDriver webDriver() {
-        // Configuration automatique du WebDriver pour Chrome
+        // Configure automatiquement le WebDriver pour Chrome
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+
+        // Configuration des options pour Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Mode headless (sans interface graphique)
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu"); // Optionnel, peut être utile selon l'environnement
+
+        // Retourne une instance de ChromeDriver avec les options configurées
+        return new ChromeDriver(options);
     }
 }
